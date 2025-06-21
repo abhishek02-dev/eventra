2ed921eb01af928fb42443a91fa4b85c502ab3cb
Abhishek Kumar <abhishekkumar2428@gmail.com>
2025-06-21 12:14:45 +0530

    Fix:

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Login from './pages/login';
 import Register from './pages/Register';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import AdminPanel from './pages/AdminPanel';
 import CreateEvent from './pages/CreateEvent';
